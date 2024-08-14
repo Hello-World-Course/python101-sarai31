55f1f71703aa1ef76c620acee332d39a294126e4
sarai <saraim009@gmail.com>
2024-08-14 23:58:03 +0300

    Fix it the file:project/ui/user_interaction.py and add a new code

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -3,15 +3,15 @@ name = input("Hello, whats your name")
 
 #Check if the name length is greater than "dani"
 if len(name) > len("dani"):
-    b_size = int(input(f"{name}, please choose board size\ninput:> "))
+    board_size = int(input(f"{name}, please choose board size\ninput:> "))
 
     # Check if board size is greater than 9
-    if b_size > 9:
-        print(f"{name}, the board size {b_size} is greater than 9.")
+    if board_size > 9:
+        print(f"{name}, the board size {board_size} is greater than 9.")
     else:
         # If the board size is 9 or less, continue asking for the number of mines
-        mines = int(input(f"{name}, for board size {b_size}, choose number of mines to allocate\ninput:> "))
-        print(f"{name}, the board size is: {b_size}, number of mines is: {mines}. ENJOY!")
+        mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate\ninput:> "))
+        print(f"{name}, the board size is: {board_size}, number of mines is: {mines}. ENJOY!")
 
 else:
     print(f"{name}, your name has fewer or equal characters than 'dani'.")
